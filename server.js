55b072b3aef6b11a0bc218fd44c6a4a0716da30e
bulbul-1234 <bulbul555rani@gmail.com>
2017-03-21 21:23:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+app.get('/article-1', function (req, res) {
+    res.send('Article-one will be requested here');
+    
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
