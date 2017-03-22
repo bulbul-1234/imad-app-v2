c678e9e58894a1eb0b1ef88d55c6ee42f8a7cd5e
bulbul-1234 <bulbul555rani@gmail.com>
2017-03-22 19:41:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+
+var element= document.getElementById( 'main-text');
+element.innetHTML ='new value';
+
+var img = document.getElementById('img');
+img.onclick = function()
+{
+    img.style.marginleft = '100px';
+    
+};
